108e85c9449f03657cf8511a0e14ba365e86a56c
BubblyFace <751835145@qq.com>
2020-02-13 22:17:03 +0800

    'longest'

---
+++ src/python/LongestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,45 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        # A loop record all the char in string, largest output is the size of dict
+        dict = {}
+        dict_len = 0
+
+        for i in str:
+            if dict[str[i]] is None:
+                dict[str[i]] = False
+                dict_len = dict_len + 1
+
+        # give a v to record longest
+        length_max = 1
+
+        #handle special
+        if dict_len is 1:
+            return  length_max
+
+        if dict_len is len(s):
+            return len(s)
+
+        # from start to record the number of unshow char, once they occur repeating. from the repeating char
+        # to start a new line.
+        current_str = []
+        current_longest = 0
+
+        for i in str:
+            if dict[str[i]] is False:
+                dict[str[i]] = True
+                current_str.append(str[i])
+                current_longest += 1
+                if current_longest > length_max:
+                    length_max = current_longest
+            else:
+                for j in current_str:
+                    dict[str[j]] = False
+                    current_str = []
+                    current_longest = 0
+        return length_max
+
+
+test = Solution()
+
+print(test.lengthOfLongestSubstring('word'))
+print(test.lengthOfLongestSubstring('hello'))
